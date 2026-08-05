bc8919a0eb74c7b8e523719da9049483954a08cb
mokshith <smokshith590@gmail.com>
2026-08-05 23:52:52 +0530

    Clean up old OneDrive directory structure

--- OneDrive/Desktop/project/KodNest-Javapractice/track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++
@@ -1,8 +0,0 @@
-
-public class WelcomeToJava {
-
-    public static void main(String[] args) {
-        System.out.println("Welcome to the Java track");
-        System.out.println("I already know how to build programming logic");
-    }
-}
